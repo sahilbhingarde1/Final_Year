2205d55fc85ee93ae3e780daaa4830abc3ceacf6
TonY <bhingardesahil41@gmail.com>
2025-09-16 23:52:15 +0530

    backend.py changes

--- Main/backend.py
+++ Main/backend.py
@@ -51,4 +51,6 @@ def generate_face():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0" , port=port)
